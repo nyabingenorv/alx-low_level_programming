0218db62c30e3cd6bacbaeb4923de3df6995971b
nyabingenorv <norvyphill@gmail.com>
2023-02-16 14:11:54 -0800

    100

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main -prints the size of various computer types
+*Return 0 success
+*/
+int main(void)
+{
+
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("size oof a char : %lu byte(s)\n",(unsigned long)sizeof(a));
+}
