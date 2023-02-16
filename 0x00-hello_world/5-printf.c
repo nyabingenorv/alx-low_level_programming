49fdea9cc072dbbace41981a55381f5c8ca05cf7
nyabingenorv <norvyphill@gmail.com>
2023-02-16 11:55:56 -0800

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'Hello, printf'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("...but the outcome is a piece of art,\n");
+return (0);
+}
+
