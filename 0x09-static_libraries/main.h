9de42866dc65eb5642f96e604e892b3f0f7a6860
nyabingenorv <norvyphill@gmail.com>
2023-03-10 12:54:24 -0800

    complete

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -25,6 +23,3 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif
-
-#endif /* MAIN_H */
-
